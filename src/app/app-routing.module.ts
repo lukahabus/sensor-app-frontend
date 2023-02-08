d1555b6e12ae2570aef8eb28aba107b1ac488e03
Luka Habuš <habusluka@gmail.com>
2023-02-08 12:00:18 +0100

    Configured environment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/sensors', pathMatch: 'full'}
+  // {path: 'movies', component: MoviesComponent},
+  // {path: 'movies/:id', component: MovieDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
